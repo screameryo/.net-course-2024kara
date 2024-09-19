95cba4fd626747350dc05c81c88e1c10f1e326a6
screameryo <screameryo13@gmail.com>
2024-09-19 14:33:17 +0300

    feat: Добавлен новый проект `TestBoxUnbox`, который содержит код для измерения времени, затраченного на операции упаковки и распаковки объектов.

---
+++ TestBoxUnbox/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Diagnostics;
+
+namespace TestBoxUnbox
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int iterations = 1000000;
+            List<int> ints = new List<int>();
+            List<object> objects = new List<object>();
+            Random random = new Random();
+
+            for (int i = 0; i < iterations; i++)
+            {
+                ints.Add(random.Next());
+            }
+
+            // Boxing
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            foreach (var i in ints)
+            {
+                objects.Add(i); //Box int in object
+            }
+            watch.Stop();
+            Console.WriteLine($"Boxing {iterations} times took: {watch.ElapsedMilliseconds} ms ({watch.ElapsedTicks} ticks)");
+
+            // Unboxing
+            watch.Restart();
+            foreach (var obj in objects)
+            {
+                int i = (int)obj; //Unbox object to int
+            }
+            watch.Stop();
+            Console.WriteLine($"Unboxing {iterations} times took: {watch.ElapsedMilliseconds} ms ({watch.ElapsedTicks} ticks)");
+                           
+            watch.Reset();
+            object boxed;
+
+            // Boxing
+            watch.Start();
+            for (int i = 0; i < iterations; i++)
+            {
+                boxed = i;
+            }
+            watch.Stop();
+            Console.WriteLine($"Boxing {iterations} times took: {watch.ElapsedMilliseconds} ms ({watch.ElapsedTicks} ticks)");
+
+            boxed = 0;
+
+            // Unboxing
+            watch.Restart();
+            for (int i = 0; i < iterations; i++)
+            {
+                int unboxed = (int)boxed; // Unboxing
+            }
+            watch.Stop();
+            Console.WriteLine($"Unboxing {iterations} times took: {watch.ElapsedMilliseconds} ms ({watch.ElapsedTicks} ticks)");
+        }
+    }
+}
