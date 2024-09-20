a08fdb359cb9d736bea611ddf83ec08f5815ca87
screameryo <screameryo13@gmail.com>
2024-09-20 09:56:40 +0300

    feat: Добавлен проект Tools и новые классы feat: Добавлены новые классы: - `Client` в файл `Client.cs`, наследуется от `Person`, включает методы для получения полного имени и возраста клиента. - `Currency` в файл `Currency.cs`, содержит свойства для имени, кода, символа и числового кода валюты. - `Employee` в файл `Employee.cs`, наследуется от `Person`, включает свойства для позиции, зарплаты, департамента и контракта сотрудника, а также методы для получения информации о сотруднике. - `Person` в файл `Person.cs`, содержит свойства для имени, фамилии, отчества, даты рождения, паспорта, телефона и адреса, а также методы для получения полного имени и возраста человека. - `BankService` в файл `BankService.cs`, включает методы для расчета зарплаты владельца банка и преобразования клиента в сотрудника. - `Program` в файл `Program.cs` в пространстве имен `Tools`, содержит метод `Main` для демонстрации работы с объектами `Person`, `Employee`, `Currency`, `Client` и `BankService`.

---
+++ BankSystem.App/Models/Currency.cs
@@ -0,0 +1,33 @@
+﻿namespace Models
+{
+    public class Currency
+    {
+        public Currency(string Name, string Code, string Symbol, int NumCode)
+        {
+            this.Name = Name;
+            this.Code = Code;
+            this.Symbol = Symbol;
+            this.NumCode = NumCode;
+        }
+
+        /// <summary>
+        /// Name of the currency
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Code of the currency
+        /// </summary>
+        public string Code { get; set; }
+
+        /// <summary>
+        /// Number code of the currency
+        /// </summary>
+        public int NumCode { get; set; }
+
+        /// <summary>
+        /// Symbol of the currency
+        /// </summary>
+        public string Symbol { get; set; }
+    }
+}
