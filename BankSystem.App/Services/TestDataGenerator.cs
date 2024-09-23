e786be261a8da6e2284b1923c0f9d0d8b0404dd2
screameryo <screameryo13@gmail.com>
2024-09-23 19:09:31 +0300

    Обновление проектов и генерация тестовых данных
    Удален контент из List.xml.
    Изменены ссылки на проекты в Bank.sln.
    Обновлены версии сборок в .csproj файлах.
    Добавлена зависимость от Bogus в BankSystem.App.csproj.
    Удалены старые примеры и добавлен новый код в Program.cs.
    Добавлен класс TestDataGenerator для генерации фейковых данных.

---
+++ BankSystem.App/Services/TestDataGenerator.cs
@@ -0,0 +1,71 @@
+﻿using BankSystem.Domain.Models;
+using Bogus;
+
+namespace BankSystem.App.Services
+{
+    public class TestDataGenerator
+    {
+        public void GenerateEmployee(ref List<Employee> employees)
+        {
+            var faker = new Faker<Employee>()
+                .RuleFor(e => e.FName, f => f.Name.FirstName())
+                .RuleFor(e => e.LName, f => f.Name.LastName())
+                .RuleFor(e => e.BDate, f => DateOnly.FromDateTime(f.Date.Past(50)))
+                .RuleFor(e => e.Passport, f => f.Random.AlphaNumeric(8))
+                .RuleFor(e => e.Telephone, f => f.Phone.PhoneNumber())
+                .RuleFor(e => e.Address, f => f.Address.FullAddress())
+                .RuleFor(e => e.Position, f => f.Name.JobTitle())
+                .RuleFor(e => e.Salary, f => f.Random.Number(10000, 100000))
+                .RuleFor(e => e.Department, f => f.Name.JobArea())
+                .RuleFor(e => e.Contract, f => f.Random.Bool() ? $"Contract to {DateTime.Now.AddMonths(f.Random.Number(1, 24)).ToString("d")}" : null);
+
+            for (int i = 0; i < 1000; i++)
+            {
+                employees.Add(faker.Generate());
+            }
+        }
+
+        public void GenerateClient(ref List<Client> clients)
+        {
+            var faker = new Faker<Client>()
+                .RuleFor(c => c.FName, f => f.Name.FirstName())
+                .RuleFor(c => c.LName, f => f.Name.LastName())
+                .RuleFor(c => c.BDate, f => DateOnly.FromDateTime(f.Date.Past(50)))
+                .RuleFor(c => c.Passport, f => f.Random.AlphaNumeric(8))
+                .RuleFor(c => c.Telephone, f => f.Phone.PhoneNumber())
+                .RuleFor(c => c.Address, f => f.Address.FullAddress())
+                .RuleFor(c => c.Ch_Account, f => f.Random.Number(100, 10000).ToString());
+
+            for (int i = 0; i < 1000; i++)
+            {
+                clients.Add(faker.Generate());
+            }
+        }
+
+        public void GenerateClientAsDictionary(ref Dictionary<string, Client> clients)
+        {
+            var faker = new Faker<Client>()
+                .RuleFor(c => c.FName, f => f.Name.FirstName())
+                .RuleFor(c => c.LName, f => f.Name.LastName())
+                .RuleFor(c => c.BDate, f => DateOnly.FromDateTime(f.Date.Past(50)))
+                .RuleFor(c => c.Passport, f => f.Random.AlphaNumeric(8))
+                .RuleFor(c => c.Telephone, f => f.Phone.PhoneNumber())
+                .RuleFor(c => c.Address, f => f.Address.FullAddress())
+                .RuleFor(c => c.Ch_Account, f => f.Random.Number(100, 10000).ToString());
+
+            for (int i = 0; i < 1000; i++)
+            {
+                var client = faker.Generate();
+                try
+                {
+                    clients.Add(client.Telephone, client);
+                }
+                catch (Exception)
+                {
+                    i--;
+                }
+                
+            }
+        }
+    }
+}
--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,5 +1,6 @@
 ﻿using BankSystem.App.Services;
 using BankSystem.Domain.Models;
+using System.Diagnostics;
 
 namespace Practice
 {
@@ -7,26 +8,21 @@ namespace Practice
     {
         static void Main(string[] args)
         {
-            // Create a person object
             var person = new Person() { FName = "John", LName = "Doe", BDate = new DateOnly(2020, 1, 1), Passport = "PMR56564", Telephone = "77712345", Address = "str. 1" };
             Console.WriteLine($"Creating a person object {person.FName} {person.LName}{Environment.NewLine}");
 
-            //Create a employee object with no contract
             var employee = new Employee() { FName = "John", LName = "Doe", Address = "str. 1", BDate = new DateOnly(2020, 1, 1), Passport = "PMR56564", Telephone = "77712345" };
             employee.Position = "Backend";
             employee.Salary = 100000;
             employee.Department = "Develop";
             Console.WriteLine($"Creating a employee object {employee.FName} {employee.LName}{Environment.NewLine}");
 
-            //Create employee contract
             employee.Contract = $"Contract to {DateTime.Now.AddMonths(24).ToString("d")}";
             Console.WriteLine($"Creating a contract for the employee {employee.Contract}{Environment.NewLine}");
 
-            //Change employee contract
             ChangeContract(ref employee);
             Console.WriteLine($"Changing the contract of the employee {employee.Contract}{Environment.NewLine}");
 
-            //Create currency object
             var currency = new Currency();//840
             currency.Name = "US Dollar";
             currency.Code = "USD";
@@ -34,12 +30,10 @@ namespace Practice
             currency.NumCode = 830;
             Console.WriteLine($"Creating a currency object {currency.Name} with Code: {currency.Code} Symbol: {currency.Symbol} NumCode: {currency.NumCode}{Environment.NewLine}");
 
-            //Change currency NumCode
             ChangeCurrency(ref currency);
             Console.WriteLine($"Changing the NumCode of the currency to {currency.NumCode}{Environment.NewLine}");
             Console.WriteLine($"Object currency is {currency.Name} with Code: {currency.Code} Symbol: {currency.Symbol} NumCode: {currency.NumCode}{Environment.NewLine}");
 
-            //Create a List of employees with positions Owner
             List<Employee> employees = new List<Employee>
             {
                 new Employee() { FName = "John", LName = "Doe", Position = "Owner", Salary = 1000000 },
@@ -47,16 +41,59 @@ namespace Practice
                 new Employee() { FName = "Janes", LName = "Doe", Position = "Backend", Salary = 100000 },
             };
 
-            //Get the owner salary
             var bankService = new BankService();
             var ownerSalary = bankService.CalculateOwnerSalary(1000000, 500000, employees);
             Console.WriteLine($"Owner salary is {ownerSalary}{Environment.NewLine}");
 
-            //Create a client object
             var client = new Client() { FName = "John", LName = "Doe", BDate = new DateOnly(2020, 1, 1), Passport = "PMR56564", Address = "str. 1", Telephone = "123456789" };
             Console.WriteLine($"Creating a client object {client.FName} {client.LName}{Environment.NewLine}");
             var newemployee = bankService.ConvertClientToEmployee(client, "Backend", "Develop", 100000);
             Console.WriteLine($"Client now is jbject employee {Environment.NewLine}");
+
+            var testDataGenerator = new TestDataGenerator();
+            List<Employee> employeesList = new List<Employee>();
+            testDataGenerator.GenerateEmployee(ref employeesList);
+            Console.WriteLine($"Generating 10000 fake employee data{Environment.NewLine}");
+
+            List<Client> clientsList = new List<Client>();
+            testDataGenerator.GenerateClient(ref clientsList);
+            Console.WriteLine($"Generating 10000 fake client data{Environment.NewLine}");
+
+            Dictionary<string, Client> clientsDictionary = new Dictionary<string, Client>();
+            testDataGenerator.GenerateClientAsDictionary(ref clientsDictionary);
+            Console.WriteLine($"Generating 10000 fake client data as dictionary{Environment.NewLine}");
+
+            Stopwatch watch = new Stopwatch();
+
+            string findtelephoneforList = clientsList[new Random().Next(0, 1000)].Telephone;
+            string findtelephonefordictionary = clientsDictionary.ElementAt(new Random().Next(0, 1000)).Key;
+
+            watch.Restart();
+            var clientSearch = clientsList.FirstOrDefault(x => x.Telephone == findtelephoneforList);
+            watch.Stop();
+            Console.WriteLine($"Search client by num telephone in list clients: {watch.ElapsedMilliseconds} ms ({watch.ElapsedTicks} ticks){Environment.NewLine}");
+
+            watch.Restart();
+            var clientSearched = clientsDictionary[findtelephonefordictionary];
+            watch.Stop();
+            Console.WriteLine($"Search client by num telephone in dictionary clientsDictionary: {watch.ElapsedMilliseconds} ms ({watch.ElapsedTicks} ticks){Environment.NewLine}");
+
+            watch.Restart();
+            var clientsAge = clientsList.Where(x => x.BDate.Year > DateTime.Now.Year - 30);
+            watch.Stop();
+            Console.WriteLine($"Search clients where age < 30: {watch.ElapsedMilliseconds} ms ({watch.ElapsedTicks} ticks). Counts: {clientsAge.Count()}{Environment.NewLine}");
+
+            watch.Restart();
+            var employeeMinSalary = employees.OrderBy(x => x.Salary).FirstOrDefault();
+            watch.Stop();
+            Console.WriteLine($"Search employee with min salary: {watch.ElapsedMilliseconds} ms ({watch.ElapsedTicks} ticks). {Environment.NewLine}");
+
+            watch.Restart();
+            var clientSearchDictionaryFirstOrDefault = clientsDictionary.FirstOrDefault(x => x.Key == findtelephonefordictionary).Value;
+            watch.Stop();
+            Console.WriteLine($"Search client in clientsDictionary by firstorddefault: {watch.ElapsedMilliseconds} ms ({watch.ElapsedTicks} ticks){Environment.NewLine}");
+
+            Console.ReadKey();
         }
 
         private static void ChangeContract(ref Employee employee)
