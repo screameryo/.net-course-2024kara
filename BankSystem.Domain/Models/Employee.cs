07bd35c041fd961d42a4ac0c2bda07713315a75b
screameryo <screameryo13@gmail.com>
2024-09-29 14:20:55 +0300

    Добавлен проект тестов и методы сравнения объектов
    Добавлен новый проект `BankSystem.Tests` в решение `Bank.sln` с конфигурациями сборки. В `TestDataGenerator.cs` добавлены методы генерации словарей клиентов и их счетов. В `Client.cs` и `Employee.cs` добавлены методы `Equals` и `GetHashCode`. Создан новый файл `Account.cs` с классом `Account` и методами `Equals` и `GetHashCode`. В `BankSystem.Tests` добавлены тесты для проверки методов `Equals` и `GetHashCode` классов `Client`, `Account` и `Employee`.

--- BankSystem.Domain/Models/Employee.cs
+++ BankSystem.Domain/Models/Employee.cs
@@ -9,5 +9,21 @@
         public string Department { get; set; }
 
         public string Contract { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            Employee e = (Employee)obj;
+            return FName == e.FName && LName == e.LName && MName == e.MName && BDate == e.BDate && Passport == e.Passport && Telephone == e.Telephone && Address == e.Address && Position == e.Position && Salary == e.Salary && Department == e.Department && Contract == e.Contract;
+        }
+
+        public override int GetHashCode()
+        {
+            return FName.GetHashCode() + LName.GetHashCode() + BDate.GetHashCode() + Passport.GetHashCode() + Telephone.GetHashCode() + Address.GetHashCode() + Position.GetHashCode() + Salary.GetHashCode() + Department.GetHashCode() + Contract.GetHashCode();
+        }
     }
 }
---
+++ BankSystem.Tests/EquivalenceTests.cs
@@ -0,0 +1,118 @@
+﻿using BankSystem.App.Services;
+using BankSystem.Domain.Models;
+using Xunit;
+
+namespace BankSystem.Tests
+{
+    public class EquivalenceTests
+    {
+        [Fact]
+        public void GetHashCodeNecessityPositivTest()
+        {
+            TestDataGenerator generator = new TestDataGenerator();
+
+            Dictionary <Client, Account> clientDictionary = generator.GenerateDictionaryClientsAndAccounts();
+
+            int rand = new Random().Next(0, 100);
+            Client newclient = new Client()
+            {
+                FName = clientDictionary.Keys.ElementAt(rand).FName,
+                LName = clientDictionary.Keys.ElementAt(rand).LName,
+                MName = clientDictionary.Keys.ElementAt(rand).MName,
+                BDate = clientDictionary.Keys.ElementAt(rand).BDate,
+                Passport = clientDictionary.Keys.ElementAt(rand).Passport,
+                Telephone = clientDictionary.Keys.ElementAt(rand).Telephone,
+                Address = clientDictionary.Keys.ElementAt(rand).Address
+            };
+
+            Assert.Equal(clientDictionary.Keys.ElementAt(rand).GetHashCode(), newclient.GetHashCode());
+        }
+
+        [Fact]
+        public void GetHashCodeNecessityPositivManyAccountsTest()
+        {
+            TestDataGenerator generator = new TestDataGenerator();
+
+            Dictionary <Client, List<Account>> clientDictionary = generator.GenerateDictionaryClientsAndManyAccounts();
+
+            int rand = new Random().Next(0, 100);
+            Client newclient = new Client()
+            {
+                FName = clientDictionary.Keys.ElementAt(rand).FName,
+                LName = clientDictionary.Keys.ElementAt(rand).LName,
+                MName = clientDictionary.Keys.ElementAt(rand).MName,
+                BDate = clientDictionary.Keys.ElementAt(rand).BDate,
+                Passport = clientDictionary.Keys.ElementAt(rand).Passport,
+                Telephone = clientDictionary.Keys.ElementAt(rand).Telephone,
+                Address = clientDictionary.Keys.ElementAt(rand).Address
+            };
+
+            Assert.Equal(clientDictionary.Keys.ElementAt(rand).GetHashCode(), newclient.GetHashCode());
+        }
+
+        [Fact]
+        public void CompareAccountsPositivTest()
+        {
+            TestDataGenerator generator = new TestDataGenerator();
+
+            Dictionary<Client, Account> clientDictionary = generator.GenerateDictionaryClientsAndAccounts();
+
+            int rand = new Random().Next(0, 100);
+            Account newAccount = new Account()
+            {
+                Currency = clientDictionary.Values.ElementAt(rand).Currency,
+                Amount = clientDictionary.Values.ElementAt(rand).Amount
+            };
+
+            Assert.Equal(clientDictionary.Values.ElementAt(rand), newAccount);
+        }
+
+        [Fact]
+        public void CompareAccountsPositivManyAccountsTest()
+        {
+            TestDataGenerator generator = new TestDataGenerator();
+
+            Dictionary<Client, List<Account>> clientDictionary = generator.GenerateDictionaryClientsAndManyAccounts();
+
+            int rand = new Random().Next(0, 100);
+
+            List<Account> newAccounts = new List<Account>();
+            foreach (var account in clientDictionary.Values.ElementAt(rand))
+            {
+                newAccounts.Add(new Account()
+                {
+                    Currency = account.Currency,
+                    Amount = account.Amount
+                });
+            }
+
+            Assert.Equal(clientDictionary.Values.ElementAt(rand), newAccounts);
+        }
+
+        [Fact]
+        public void CompareEmployeePositivTest()
+        {
+            TestDataGenerator generator = new TestDataGenerator();
+
+            List<Employee> employees = generator.GenerateEmployee();
+
+            int rand = new Random().Next(0, 1000);
+
+            Employee newEmployee = new Employee()
+            {
+                FName = employees[rand].FName,
+                LName = employees[rand].LName,
+                MName = employees[rand].MName,
+                BDate = employees[rand].BDate,
+                Passport = employees[rand].Passport,
+                Telephone = employees[rand].Telephone,
+                Address = employees[rand].Address,
+                Position = employees[rand].Position,
+                Department = employees[rand].Department,
+                Salary = employees[rand].Salary
+            };
+
+            Assert.Equal(employees[rand], newEmployee);
+        }
+    }
+}
